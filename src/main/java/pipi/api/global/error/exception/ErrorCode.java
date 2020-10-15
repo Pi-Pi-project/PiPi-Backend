6ec83958f6914012bafe56a7776882cb3f686282
yangseungbin <tanja1206@naver.com>
2020-10-15 16:12:25 +0900

    [ADD] 401, 409

--- src/main/java/pipi/api/global/error/exception/ErrorCode.java
+++ src/main/java/pipi/api/global/error/exception/ErrorCode.java
@@ -9,7 +9,9 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     BAD_REQUEST(400, "Bad Request"),
-    USER_NOT_FOUND(404, "User Not Found");
+    USER_NOT_FOUND(404, "User Not Found"),
+    INVALID_AUTH_CODE(401, "Invalid Auth Code"),
+    USER_ALREADY_EXIST(409, "User Already Exist");
 
     private final int status;
     private final String message;
