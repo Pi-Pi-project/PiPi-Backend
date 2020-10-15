41e067739d8d252c194de92b552bf02e90584b91
yangseungbin <tanja1206@naver.com>
2020-10-15 13:21:06 +0900

    [ADD] project init

---
+++ src/test/java/pipi/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pipi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
