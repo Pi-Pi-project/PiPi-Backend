361e073e85848a82b946bd9d548ae8ea84df3dc5
yangseungbin <tanja1206@naver.com>
2020-10-15 17:03:21 +0900

    [ADD] verify auth code

--- src/main/java/pipi/api/domain/user/controller/UserController.java
+++ src/main/java/pipi/api/domain/user/controller/UserController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pipi.api.domain.user.dto.EmailSendRequest;
+import pipi.api.domain.user.dto.EmailVerifyRequest;
 import pipi.api.domain.user.service.UserService;
-
-import javax.validation.constraints.Email;
 
 @RestController
 @RequestMapping("/user")
@@ -19,5 +18,10 @@ public class UserController {
     @PostMapping("/email")
     public void sendEmail(@RequestBody EmailSendRequest emailSendRequest) {
         userService.sendEmail(emailSendRequest);
+    }
+
+    @PostMapping("/email/check")
+    public void verifyCode(@RequestBody EmailVerifyRequest emailVerifyRequest) {
+        userService.verifyCode(emailVerifyRequest);
     }
 }
