9b5c141d968d9186324cd3a54e7e737e587ca19e
yangseungbin <tanja1206@naver.com>
2020-10-15 13:39:32 +0900

    [ADD] user entity

---
+++ src/main/java/pipi/api/domain/user/domain/User.java
@@ -0,0 +1,45 @@
+package pipi.api.domain.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import pipi.api.domain.post.domain.Post;
+import pipi.api.domain.user.domain.enums.Admin;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table
+@Entity
+@Builder
+public class User {
+    @Id
+    @Column(length = 50, nullable = false)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Column(length = 20, nullable = false)
+    private String nickname;
+
+    @Column(columnDefinition = "varchar(50) default 'userDefault.jpg'")
+    private String profileImg;
+
+    @Column(length = 100)
+    private String gitUrl;
+
+    @Column(length = 200)
+    private String introduce;
+
+    @Column(length = 30)
+    @Enumerated(EnumType.STRING)
+    private Admin admin;
+
+    @OneToMany(mappedBy = "userEmail", cascade = CascadeType.ALL)
+    private List<Post> posts;
+}
