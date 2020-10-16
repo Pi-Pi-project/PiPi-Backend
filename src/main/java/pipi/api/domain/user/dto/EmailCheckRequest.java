9a46f34cabc8fcc80211fffa9a1dcb319fe532c6
yangseungbin <tanja1206@naver.com>
2020-10-17 02:22:46 +0900

    [ADD] email check request dto

---
+++ src/main/java/pipi/api/domain/user/dto/EmailCheckRequest.java
@@ -0,0 +1,18 @@
+package pipi.api.domain.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class EmailCheckRequest {
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String code;
+}
