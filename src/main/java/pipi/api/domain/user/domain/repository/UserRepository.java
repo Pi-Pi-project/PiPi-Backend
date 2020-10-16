c9ff4fbd6b14241cd8057928ff1bafa37a54e9a6
yangseungbin <tanja1206@naver.com>
2020-10-17 01:39:49 +0900

    [ADD] user repository

---
+++ src/main/java/pipi/api/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package pipi.api.domain.user.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pipi.api.domain.user.domain.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String username);
+}
