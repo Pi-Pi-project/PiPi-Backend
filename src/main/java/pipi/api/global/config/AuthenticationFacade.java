18d3d5f8b341ce44a2a2becf69b73ff217743643
yangseungbin <tanja1206@naver.com>
2020-10-17 01:42:14 +0900

    [ADD] jwt decode

---
+++ src/main/java/pipi/api/global/config/AuthenticationFacade.java
@@ -0,0 +1,16 @@
+package pipi.api.global.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFacade {
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    public String getUserEmail() {
+        return this.getAuthentication().getName();
+    }
+}
