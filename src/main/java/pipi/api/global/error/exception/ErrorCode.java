7c2649e946c5495f7f13a285658f8cda3a7ec674
yangseungbin <tanja1206@naver.com>
2020-10-17 01:41:43 +0900

    [ADD] error code

---
+++ src/main/java/pipi/api/global/error/exception/ErrorCode.java
@@ -0,0 +1,19 @@
+package pipi.api.global.error.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    INVALID_AUTH_EMAIL(401,"Invalid Auth Email"),
+    INVALID_AUTH_CODE(401,"Invalid Auth Code"),
+    USER_NOT_FOUND(404, "User Not Found"),
+    USER_ALREADY_EXISTS(409, "User is Already Exists");
+
+    private final int status;
+    private final String message;
+
+}
