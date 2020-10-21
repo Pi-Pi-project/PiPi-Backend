4dad5de6b15b78048d666ea0ec7f0539b7dfa525
yangseungbin <tanja1206@naver.com>
2020-10-21 20:21:29 +0900

    [ADD] setter

--- src/main/java/pipi/api/domain/auth/dto/RefreshResponse.java
+++ src/main/java/pipi/api/domain/auth/dto/RefreshResponse.java
@@ -2,8 +2,10 @@ package pipi.api.domain.auth.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
 public class RefreshResponse {
     private String accessToken;
