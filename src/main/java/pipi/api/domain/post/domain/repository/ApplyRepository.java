480da9c0926c76d57d7eb314b12c36213e25a05c
yangseungbin <tanja1206@naver.com>
2020-10-25 15:26:56 +0900

    [ADD] findAllByPostId

--- src/main/java/pipi/api/domain/post/domain/repository/ApplyRepository.java
+++ src/main/java/pipi/api/domain/post/domain/repository/ApplyRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pipi.api.domain.post.domain.Apply;
 import pipi.api.domain.post.domain.ApplyPK;
 
+import java.util.List;
+
 public interface ApplyRepository extends JpaRepository<Apply, ApplyPK> {
+    List<Apply> findAllByPostId(Long id);
 }
