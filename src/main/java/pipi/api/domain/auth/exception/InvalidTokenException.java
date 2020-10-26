4bd102747772c39c2d45b0c5b124ffa00071b890
yangseungbin <tanja1206@naver.com>
2020-10-26 10:10:51 +0900

    [DEL] import

--- src/main/java/pipi/api/domain/auth/exception/InvalidTokenException.java
+++ src/main/java/pipi/api/domain/auth/exception/InvalidTokenException.java
@@ -1,6 +1,5 @@
 package pipi.api.domain.auth.exception;
 
-import pipi.api.domain.user.exception.InvalidAuthEmailException;
 import pipi.api.global.error.exception.BusinessException;
 import pipi.api.global.error.exception.ErrorCode;
 
