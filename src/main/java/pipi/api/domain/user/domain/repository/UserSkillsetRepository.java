cfcc6e9d67ea1f4c3bdfce39fa284253c82fd56c
yangseungbin <tanja1206@naver.com>
2020-10-27 22:41:18 +0900

    [ADD] findAllByUserEmail

--- src/main/java/pipi/api/domain/user/domain/repository/UserSkillsetRepository.java
+++ src/main/java/pipi/api/domain/user/domain/repository/UserSkillsetRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pipi.api.domain.user.domain.UserSkillset;
 import pipi.api.domain.user.domain.UserSkillsetPK;
 
+import java.util.List;
+
 public interface UserSkillsetRepository extends JpaRepository<UserSkillset, UserSkillsetPK> {
+    List<UserSkillset> findAllByUserEmail(String userEmail);
 }
