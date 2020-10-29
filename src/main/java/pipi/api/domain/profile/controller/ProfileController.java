a74a59c71fed9dd5d168251d95f8cc385ec26433
yangseungbin <tanja1206@naver.com>
2020-10-29 21:42:02 +0900

    [ADD] get portfolios

--- src/main/java/pipi/api/domain/profile/controller/ProfileController.java
+++ src/main/java/pipi/api/domain/profile/controller/ProfileController.java
@@ -2,8 +2,13 @@ package pipi.api.domain.profile.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import pipi.api.domain.profile.domain.Portfolio;
+import pipi.api.domain.profile.dto.AddPortfolioRequest;
 import pipi.api.domain.profile.dto.ShowProfileResponse;
 import pipi.api.domain.profile.service.ProfileService;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/profile")
@@ -13,7 +18,16 @@ public class ProfileController {
 
     @GetMapping
     public ShowProfileResponse getProfile(@RequestParam(value = "email", required = true) String userEmail) {
-        System.out.println(userEmail);
         return profileService.getProfile(userEmail);
+    }
+
+    @GetMapping("/portfolio")
+    public List<Portfolio> getPortfoli  os() {
+        return profileService.getPortfolios();
+    }
+
+    @PostMapping("/portforlio")
+    public void addPortfolio(@RequestBody @Valid AddPortfolioRequest addPortfolioRequest) {
+        profileService.addPortfolio(addPortfolioRequest);
     }
 }
