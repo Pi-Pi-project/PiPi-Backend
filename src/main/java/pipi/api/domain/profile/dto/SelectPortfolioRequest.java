c631b7bedf70990997842fd4c857ba63dcb44e1a
yangseungbin <tanja1206@naver.com>
2020-11-01 22:29:34 +0900

    [ADD] select portfolio request

---
+++ src/main/java/pipi/api/domain/profile/dto/SelectPortfolioRequest.java
@@ -0,0 +1,11 @@
+package pipi.api.domain.profile.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SelectPortfolioRequest {
+    private Long first;
+    private Long second;
+}
