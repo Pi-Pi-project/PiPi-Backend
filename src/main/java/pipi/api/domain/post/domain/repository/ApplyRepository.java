81715f979dd436b131bfa3b800e532afade34160
yangseungbin <tanja1206@naver.com>
2020-11-03 22:52:16 +0900

    [ADD] findAllByUserEmail

--- src/main/java/pipi/api/domain/post/domain/repository/ApplyRepository.java
+++ src/main/java/pipi/api/domain/post/domain/repository/ApplyRepository.java
@@ -1,5 +1,7 @@
 package pipi.api.domain.post.domain.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pipi.api.domain.post.domain.Apply;
@@ -11,4 +13,6 @@ import java.util.List;
 public interface ApplyRepository extends JpaRepository<Apply, ApplyPK> {
     List<Apply> findAllByPostId(Long id);
     Apply findByPostIdAndUserEmail(Long id, String userEmail);
+    void deleteByPostIdAndUserEmail(Long id, String userEmail);
+    Page<Apply> findAllByUserEmail(Pageable pageable, String userEmail);
 }
