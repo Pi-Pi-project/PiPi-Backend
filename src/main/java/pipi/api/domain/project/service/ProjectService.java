56ce1cf122505ec96d26b0b257927cbe4960e9dd
yangseungbin <tanja1206@naver.com>
2020-11-09 23:26:30 +0900

    [ADD] get my project service

--- src/main/java/pipi/api/domain/project/service/ProjectService.java
+++ src/main/java/pipi/api/domain/project/service/ProjectService.java
@@ -1,7 +1,12 @@
 package pipi.api.domain.project.service;
 
+import org.springframework.data.domain.Pageable;
 import pipi.api.domain.project.dto.CreateProjectRequest;
+import pipi.api.domain.project.dto.GetMyProjectResponse;
+
+import java.util.List;
 
 public interface ProjectService {
     void createProject(CreateProjectRequest createProjectRequest);
+    List<GetMyProjectResponse> getMyProject(Pageable pageable);
 }
--- src/main/java/pipi/api/domain/project/service/ProjectServiceImpl.java
+++ src/main/java/pipi/api/domain/project/service/ProjectServiceImpl.java
@@ -1,12 +1,17 @@
 package pipi.api.domain.project.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import pipi.api.domain.post.domain.Apply;
 import pipi.api.domain.post.domain.Post;
+import pipi.api.domain.post.domain.PostSkillset;
 import pipi.api.domain.post.domain.enums.Accept;
 import pipi.api.domain.post.domain.repository.ApplyRepository;
 import pipi.api.domain.post.domain.repository.PostRepository;
+import pipi.api.domain.post.domain.repository.PostSkillsetRepository;
+import pipi.api.domain.post.dto.GetPostsResponse;
 import pipi.api.domain.post.exception.PostNotFoundException;
 import pipi.api.domain.project.domain.Member;
 import pipi.api.domain.project.domain.Project;
@@ -14,10 +19,15 @@ import pipi.api.domain.project.domain.enums.MemberStatus;
 import pipi.api.domain.project.domain.repository.MemberRepository;
 import pipi.api.domain.project.domain.repository.ProjectRepository;
 import pipi.api.domain.project.dto.CreateProjectRequest;
+import pipi.api.domain.project.dto.GetMyProjectResponse;
 import pipi.api.domain.project.exception.TooManyMemberException;
+import pipi.api.domain.user.domain.User;
+import pipi.api.domain.user.domain.repository.UserRepository;
 import pipi.api.global.config.AuthenticationFacade;
+import pipi.api.global.error.exception.UserNotFoundException;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,6 +35,8 @@ import java.util.List;
 public class ProjectServiceImpl implements ProjectService {
     private final PostRepository postRepository;
     private final ProjectRepository projectRepository;
+    private final PostSkillsetRepository postSkillsetRepository;
+    private final UserRepository userRepository;
     private final AuthenticationFacade authenticationFacade;
     private final ApplyRepository applyRepository;
     private final MemberRepository memberRepository;
@@ -61,5 +73,33 @@ public class ProjectServiceImpl implements ProjectService {
             );
         }
         postRepository.deleteById(postId);
+    }
+
+    @Override
+    public List<GetMyProjectResponse> getMyProject(Pageable pageable) {
+        Page<Member> members = memberRepository.findAllByUserEmail(authenticationFacade.getUserEmail(), pageable);
+        List<GetMyProjectResponse> projectResponseList = new ArrayList<>();
+        for (Member member : members) {
+            Post post = postRepository.findById(member.getProjectId())
+                    .orElseThrow(PostNotFoundException::new);
+            List<PostSkillset> skills = postSkillsetRepository.findAllByPostId(post.getId());
+            User writer = userRepository.findByEmail(post.getUserEmail())
+                    .orElseThrow(UserNotFoundException::new);
+            projectResponseList.add(
+                    GetMyProjectResponse.builder()
+                            .id(post.getId())
+                            .title(post.getTitle())
+                            .img(post.getImg())
+                            .category(post.getCategory())
+                            .idea(post.getIdea())
+                            .postSkillsets(skills)
+                            .userEmail(writer.getEmail())
+                            .userImg(writer.getProfileImage())
+                            .userNickname(writer.getNickname())
+                            .createdAt(post.getCreatedAt())
+                            .build()
+            );
+        }
+        return projectResponseList;
     }
 }
