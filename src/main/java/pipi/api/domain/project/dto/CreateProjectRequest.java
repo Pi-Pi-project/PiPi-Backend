cd92e169106b277f3a0c8bce09730c8ce5425cbb
yangseungbin <tanja1206@naver.com>
2020-11-16 08:39:22 +0900

    [ADD] not blank not null

--- src/main/java/pipi/api/domain/project/dto/CreateProjectRequest.java
+++ src/main/java/pipi/api/domain/project/dto/CreateProjectRequest.java
@@ -2,7 +2,10 @@ package pipi.api.domain.project.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 public class CreateProjectRequest {
+    @NotNull
     private Long postId;
 }
--- src/main/java/pipi/api/domain/project/dto/CreateTodoRequest.java
+++ src/main/java/pipi/api/domain/project/dto/CreateTodoRequest.java
@@ -2,9 +2,17 @@ package pipi.api.domain.project.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 public class CreateTodoRequest {
+    @NotBlank
     private String todo;
+
+    @NotBlank
     private String date;
+
+    @NotNull
     private Long projectId;
 }
