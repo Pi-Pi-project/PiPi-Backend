38f81eebfda39e322efbd7446570cfeffc3a8710
yangseungbin <tanja1206@naver.com>
2020-11-16 09:01:38 +0900

    [ADD] findByUserEmailAndProjectId

--- src/main/java/pipi/api/domain/project/domain/repository/MemberRepository.java
+++ src/main/java/pipi/api/domain/project/domain/repository/MemberRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pipi.api.domain.project.domain.Member;
+import pipi.api.domain.project.domain.MemberPK;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, MemberPK> {
     Page<Member> findAllByUserEmail(String userEmail, Pageable pageable);
+    Member findByUserEmailAndProjectId(String userEmail, Long projectId);
 }
