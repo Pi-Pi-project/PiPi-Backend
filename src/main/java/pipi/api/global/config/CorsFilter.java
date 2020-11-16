ee292c97498a0d89e436abf947b0e5bc2406c0ab
yangseungbin <tanja1206@naver.com>
2020-11-16 22:04:36 +0900

    [DEL] 주석 제거

--- src/main/java/pipi/api/global/config/CorsFilter.java
+++ src/main/java/pipi/api/global/config/CorsFilter.java
@@ -9,9 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * CORS 설정
- */
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
