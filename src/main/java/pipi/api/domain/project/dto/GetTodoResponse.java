53966a5ff51a75f23c0747dbc437b024e2fbe6dd
yangseungbin <tanja1206@naver.com>
2020-11-16 15:02:29 +0900

    [ADD] get todo response

--- src/main/java/pipi/api/domain/project/dto/GetTodoResponse.java
+++ src/main/java/pipi/api/domain/project/dto/GetTodoResponse.java
@@ -7,6 +7,7 @@ import pipi.api.domain.project.domain.enums.TodoStatus;
 @Getter
 @Builder
 public class GetTodoResponse {
+    private final Long id;
     private final String nickname;
     private final String date;
     private final String todo;
