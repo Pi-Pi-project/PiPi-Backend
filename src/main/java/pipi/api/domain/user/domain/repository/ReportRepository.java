a0f6d3ee12497ae9c2453a88577da1ca27da158a
yangseungbin <tanja1206@naver.com>
2020-11-18 22:52:53 +0900

    [ADD] report repository

---
+++ src/main/java/pipi/api/domain/user/domain/repository/ReportRepository.java
@@ -0,0 +1,10 @@
+package pipi.api.domain.user.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pipi.api.domain.user.domain.Report;
+import pipi.api.domain.user.domain.ReportPK;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, ReportPK> {
+}
