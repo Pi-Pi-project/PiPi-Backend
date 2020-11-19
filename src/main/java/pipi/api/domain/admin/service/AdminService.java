f43d9fd6e9e9442afb8f28d7f52c133b39b28b66
yangseungbin <tanja1206@naver.com>
2020-11-19 16:00:59 +0900

    [ADD] get report users service

--- src/main/java/pipi/api/domain/admin/service/AdminService.java
+++ src/main/java/pipi/api/domain/admin/service/AdminService.java
@@ -1,8 +1,15 @@
 package pipi.api.domain.admin.service;
 
+import org.springframework.data.domain.Pageable;
+import pipi.api.domain.admin.dto.GetApprovalProjectsResponse;
+import pipi.api.domain.admin.dto.GetReportUsersResponse;
 import pipi.api.domain.auth.dto.UserLoginRequest;
 import pipi.api.domain.user.dto.TokenResponse;
 
+import java.util.List;
+
 public interface AdminService {
     TokenResponse adminLogin(UserLoginRequest userLoginRequest);
+    List<GetApprovalProjectsResponse> getApprovalProjects(Pageable pageable);
+    List<GetReportUsersResponse> getReportUsers(Pageable pageable);
 }
--- src/main/java/pipi/api/domain/admin/service/AdminServiceImpl.java
+++ src/main/java/pipi/api/domain/admin/service/AdminServiceImpl.java
@@ -2,21 +2,38 @@ package pipi.api.domain.admin.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import pipi.api.domain.admin.dto.GetApprovalProjectsResponse;
+import pipi.api.domain.admin.dto.GetReportUsersResponse;
 import pipi.api.domain.admin.exception.NotAdminException;
 import pipi.api.domain.auth.dto.UserLoginRequest;
+import pipi.api.domain.project.domain.Approval;
+import pipi.api.domain.project.domain.Project;
+import pipi.api.domain.project.domain.repository.ApprovalRepository;
+import pipi.api.domain.project.domain.repository.ProjectRepository;
+import pipi.api.domain.project.exception.ProjectNotFoundException;
+import pipi.api.domain.user.domain.Report;
 import pipi.api.domain.user.domain.User;
 import pipi.api.domain.user.domain.enums.Admin;
+import pipi.api.domain.user.domain.repository.ReportRepository;
 import pipi.api.domain.user.domain.repository.UserRepository;
 import pipi.api.domain.user.dto.TokenResponse;
 import pipi.api.global.config.security.JwtTokenProvider;
 import pipi.api.global.error.exception.UserNotFoundException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AdminServiceImpl implements AdminService {
     private final UserRepository userRepository;
+    private final ApprovalRepository approvalRepository;
+    private final ProjectRepository projectRepository;
+    private final ReportRepository reportRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
 
@@ -31,6 +48,42 @@ public class AdminServiceImpl implements AdminService {
         if (user.getAdmin() != Admin.ADMIN)
             throw new NotAdminException();
         return responseToken(userLoginRequest.getEmail());
+    }
+
+    @Override
+    public List<GetApprovalProjectsResponse> getApprovalProjects(Pageable pageable) {
+        List<GetApprovalProjectsResponse> getApprovalProjectsResponses = new ArrayList<>();
+        Page<Approval> approvals = approvalRepository.findAll(pageable);
+        for (Approval approval : approvals) {
+            Project project = projectRepository.findById(approval.getProjectId())
+                    .orElseThrow(ProjectNotFoundException::new);
+            getApprovalProjectsResponses.add(
+                    GetApprovalProjectsResponse.builder()
+                            .projectId(project.getId())
+                            .title(project.getTitle())
+                            .coverImg(project.getImg())
+                            .build()
+            );
+        }
+        return getApprovalProjectsResponses;
+    }
+
+    @Override
+    public List<GetReportUsersResponse> getReportUsers(Pageable pageable) {
+        List<GetReportUsersResponse> getReportUsersResponses = new ArrayList<>();
+        Page<Report> reports = reportRepository.findAll(pageable);
+        for (Report report : reports) {
+            User user = userRepository.findByEmail(report.getReportedEmail())
+                    .orElseThrow(UserNotFoundException::new);
+            getReportUsersResponses.add(
+                    GetReportUsersResponse.builder()
+                            .userEmail(user.getEmail())
+                            .profileImg(user.getProfileImage())
+                            .userNickname(user.getNickname())
+                            .build()
+            );
+        }
+        return getReportUsersResponses;
     }
 
     private TokenResponse responseToken(String email) {
