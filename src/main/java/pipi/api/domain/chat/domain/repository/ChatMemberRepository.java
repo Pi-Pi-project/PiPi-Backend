675248d9342a38056860479623c2c5286054f770
yangseungbin <tanja1206@naver.com>
2020-11-28 23:11:56 +0900

    [ADD] findAllByUserEmail

--- src/main/java/pipi/api/domain/chat/domain/repository/ChatMemberRepository.java
+++ src/main/java/pipi/api/domain/chat/domain/repository/ChatMemberRepository.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Repository;
 import pipi.api.domain.chat.domain.ChatMember;
 import pipi.api.domain.chat.domain.ChatMemberPK;
 
+import java.util.List;
+
 @Repository
 public interface ChatMemberRepository extends JpaRepository<ChatMember, ChatMemberPK> {
+    List<ChatMember> findAllByUserEmail(String userEmail);
 }
