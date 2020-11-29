1618b6e825cc864487d6c98e2530dbc4268fb80e
yangseungbin <tanja1206@naver.com>
2020-11-30 08:47:36 +0900

    [ADD] get chats service

---
+++ src/main/java/pipi/api/domain/chat/service/ChatService.java
@@ -0,0 +1,10 @@
+package pipi.api.domain.chat.service;
+
+import org.springframework.data.domain.Pageable;
+import pipi.api.domain.chat.dto.GetChatsResponse;
+
+import java.util.List;
+
+public interface ChatService {
+    List<GetChatsResponse> getChats(Long id, Pageable pageable);
+}
---
+++ src/main/java/pipi/api/domain/chat/service/ChatServiceImpl.java
@@ -0,0 +1,48 @@
+package pipi.api.domain.chat.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import pipi.api.domain.chat.domain.Chat;
+import pipi.api.domain.chat.domain.repository.ChatRepository;
+import pipi.api.domain.chat.dto.GetChatsResponse;
+import pipi.api.domain.user.domain.User;
+import pipi.api.domain.user.domain.repository.UserRepository;
+import pipi.api.global.config.security.AuthenticationFacade;
+import pipi.api.global.error.exception.UserNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatServiceImpl implements ChatService {
+    private ChatRepository chatRepository;
+    private UserRepository userRepository;
+    private AuthenticationFacade authenticationFacade;
+
+    @Override
+    public List<GetChatsResponse> getChats(Long id, Pageable pageable) {
+        Page<Chat> chats = chatRepository.findAllByRoomIdOrderById(id, pageable);
+        List<GetChatsResponse> chatsResponses = new ArrayList<>();
+        for (Chat chat : chats) {
+            User user = userRepository.findByEmail(chat.getUserEmail())
+                    .orElseThrow(UserNotFoundException::new);
+            if (chat.getUserEmail() == authenticationFacade.getUserEmail()) {
+                chatsResponses.add(
+                        GetChatsResponse.builder()
+                                .userNickname(user.getNickname())
+                                .message(chat.getMessage())
+                                .isMine(true)
+                                .build()
+                );
+            } else {
+                chatsResponses.add(
+                        GetChatsResponse.builder()
+                                .userNickname(user.getNickname())
+                                .message(chat.getMessage())
+                                .isMine(false)
+                                .build()
+                );
+            }
+        }
+        return chatsResponses;
+    }
+}
