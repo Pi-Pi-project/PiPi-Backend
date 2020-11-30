3bff5f1a0e3a343d034af59b35a6fc787c9a993a
yangseungbin <tanja1206@naver.com>
2020-11-30 22:40:14 +0900

    [ADD] required args constructor annotation

--- src/main/java/pipi/api/domain/chat/service/ChatServiceImpl.java
+++ src/main/java/pipi/api/domain/chat/service/ChatServiceImpl.java
@@ -1,7 +1,9 @@
 package pipi.api.domain.chat.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import pipi.api.domain.chat.domain.Chat;
 import pipi.api.domain.chat.domain.repository.ChatRepository;
 import pipi.api.domain.chat.dto.GetChatsResponse;
@@ -13,10 +15,12 @@ import pipi.api.global.error.exception.UserNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class ChatServiceImpl implements ChatService {
-    private ChatRepository chatRepository;
-    private UserRepository userRepository;
-    private AuthenticationFacade authenticationFacade;
+    private final ChatRepository chatRepository;
+    private final UserRepository userRepository;
+    private final AuthenticationFacade authenticationFacade;
 
     @Override
     public List<GetChatsResponse> getChats(Long id, Pageable pageable) {
