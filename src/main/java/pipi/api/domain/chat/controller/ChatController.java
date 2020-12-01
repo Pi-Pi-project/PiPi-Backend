351515b57faecb642c5fd295cc869e843dca0bc5
yangseungbin <tanja1206@naver.com>
2020-12-01 19:05:48 +0900

    [ADD] get individual chat

--- src/main/java/pipi/api/domain/chat/controller/ChatController.java
+++ src/main/java/pipi/api/domain/chat/controller/ChatController.java
@@ -3,11 +3,9 @@ package pipi.api.domain.chat.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pipi.api.domain.chat.dto.GetChatsResponse;
+import pipi.api.domain.chat.dto.IndividualChatResponse;
 import pipi.api.domain.chat.service.ChatService;
 
 import java.util.List;
@@ -21,5 +19,10 @@ public class ChatController {
     @GetMapping("/{id}")
     public List<GetChatsResponse> getChats(@PathVariable Long id, @PageableDefault(size = 10) Pageable pageable) {
         return chatService.getChats(id, pageable);
+    }
+
+    @GetMapping
+    public IndividualChatResponse getIndividual(@RequestParam(value = "email") String userEmail) {
+        return chatService.individualChat(userEmail);
     }
 }
