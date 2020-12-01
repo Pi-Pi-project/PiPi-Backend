7ec23f8e3f7e8c6a9f2f3cc7c2cfdf90476ca44e
yangseungbin <tanja1206@naver.com>
2020-12-01 19:05:32 +0900

    [ADD] find by room id

--- src/main/java/pipi/api/domain/chat/domain/repository/ChatMemberRepository.java
+++ src/main/java/pipi/api/domain/chat/domain/repository/ChatMemberRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface ChatMemberRepository extends JpaRepository<ChatMember, ChatMemberPK> {
     List<ChatMember> findAllByUserEmail(String userEmail);
+    ChatMember findByRoomId(Long id);
 }
