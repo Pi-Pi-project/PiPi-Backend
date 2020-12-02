bd30595b02c09baf1b300942f6ac512e7a2d7ca9
yangseungbin <tanja1206@naver.com>
2020-12-02 23:33:19 +0900

    [ADD] email parameter

--- src/main/java/pipi/api/domain/user/dto/PasswordChangeRequest.java
+++ src/main/java/pipi/api/domain/user/dto/PasswordChangeRequest.java
@@ -2,10 +2,15 @@ package pipi.api.domain.user.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Getter
 public class PasswordChangeRequest {
+    @Email
+    @NotBlank
+    private String email;
+
     @NotBlank
     private String password;
 }
